a0cfb2b323a0f2c2a98c1bd4d07a0c22115a2525
_ <pedropmancillac@gmail.com>
2023-07-08 21:10:33 -0400

    fix header no active link

--- public/assets/js/components/header.js
+++ public/assets/js/components/header.js
@@ -1,5 +1,6 @@
 export default () => {
     const activeLink = document.querySelector(`.nav-link[href="${window.location.pathname}"]`);
+    if (!activeLink) return;
     activeLink.classList.add('active', "disabled");
     activeLink.setAttribute("aria-current", "page");
 };
